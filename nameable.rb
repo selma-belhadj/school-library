47e982a321137794a022c7ba3930015390c24ed6
selma <belhadjselma1@gmail.com>
2022-06-17 14:51:29 +0100

    use decorator design pattern to correct the names of people.

---
+++ nameable.rb
@@ -0,0 +1,30 @@
+class Nameable
+  def correct_name
+    raise NotImplementedError, 'You must implement the correct_name method'
+  end
+end
+
+class BaseDecorator < Nameable
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+class CapitalizeDecorator < BaseDecorator
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
+end
+
+class TrimmerDecorator < BaseDecorator
+  def correct_name
+    return @nameable.correct_name if @nameable.correct_name.length <= 10
+
+    @nameable.correct_name[0, 10]
+  end
+end
