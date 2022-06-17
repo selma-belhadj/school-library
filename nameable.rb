8c9afef55984c966ba70e8885104dcc7b2cd1e91
selma <belhadjselma1@gmail.com>
2022-06-17 16:36:53 +0100

    use the Ternary operator

--- nameable.rb
+++ nameable.rb
@@ -23,8 +23,6 @@ end
 
 class TrimmerDecorator < BaseDecorator
   def correct_name
-    return @nameable.correct_name if @nameable.correct_name.length <= 10
-
-    @nameable.correct_name[0, 10]
+    @nameable.correct_name.length > 10 ? @nameable.correct_name[0..9] : @nameable.correct_name
   end
 end
