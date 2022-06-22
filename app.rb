742afa94bb04d5336758bf02d3904fc28e55ce04
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-06-22 12:55:44 +0300

    Update removed persons functionality from app.rb file

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require './apps/books_app'
+require './apps/persons_app'
 require './classes/nameable'
 require './classes/person'
 require './classes/teacher'
@@ -8,11 +9,11 @@ require './classes/book'
 require './classes/rental'
 
 class App
-  attr_accessor :books
+  attr_accessor :books, :persons
 
   def initialize
     @books = BooksApp.new
-    @persons = []
+    @persons = PersonApp.new
     @rentals = []
   end
 
@@ -39,9 +40,9 @@ class App
     when '1'
       @books.list_books
     when '2'
-      list_persons
+      @persons.list_persons
     when '3'
-      add_person
+      @persons.add_person
     when '4'
       @books.add_book
     when '5'
@@ -61,63 +62,6 @@ class App
       input = gets.chomp
       option(input)
     end
-  end
-
-  def list_persons
-    puts 'there is no person registred in the library' if @persons.empty?
-    @persons.each_with_index do |person, index|
-      puts "[#{person.class.name}] Index: #{index}, Name: #{person.name}, Age: #{person.age}, id: #{person.id} "
-    end
-  end
-
-  def add_person
-    print 'To create a student, press 1, to create a teacher, press 2 : '
-    option = gets.chomp
-    case option
-    when '1'
-      create_student
-    when '2'
-      create_teacher
-    else
-      puts 'Invalid input. Try again'
-    end
-  end
-
-  def create_student
-    puts '********** Create a new student **********'
-    print 'Enter student name: '
-    name = gets.chomp
-    print 'Enter student age: '
-    age = gets.chomp.to_i
-    print 'Has parent permission? [Y/N]: '
-    parent_permission = gets.chomp.downcase
-    case parent_permission
-    when 'y'
-      classroom = 'classroom A'
-      student = Student.new(age, classroom, name, parent_permission: true)
-    when 'n'
-      classroom = 'classroom B'
-      student = Student.new(age, classroom, name, parent_permission: false)
-    else
-      puts 'Please enter y for yes or n for no.'
-      create_student
-    end
-    @persons << student
-    puts '********** Student created successfully **********'
-  end
-
-  def create_teacher
-    puts '********** Create a new teacher **********'
-    print 'Enter teacher name: '
-    name = gets.chomp
-    print 'Enter teacher age: '
-    age = gets.chomp.to_i
-    print 'Enter teacher specialization: '
-    specialization = gets.chomp
-    teacher = Teacher.new(age, specialization, name)
-    @persons << teacher
-    puts ''
-    puts '********** Teacher created successfully **********'
   end
 
   def add_rental
@@ -148,26 +92,5 @@ class App
         puts "Book #{rental.book.title} has been rented to #{rental.person.name} on date #{rental.date}"
       end
     end
-  end
-
-  def save_people(file_name)
-    ppl = []
-    @persons.each do |person|
-      ppl << if person.instance_of? Student
-               {
-                 role: person.class.name,
-                 name: person.name,
-                 age: person.age
-               }
-             elsif person.instance_of? Teacher
-               {
-                 role: person.class.name,
-                 name: person.name,
-                 age: person.age,
-                 specialization: person.specialization
-               }
-             end
-    end
-    File.write(file_name, JSON.generate(ppl).to_s)
   end
 end
