76a9e641eab4106ffc3a9cea8b63907da7d147eb
selma <belhadjselma1@gmail.com>
2022-06-22 11:20:58 +0100

    1- checked and fixed offences and 2-update the handle_data file by adding save_people & read_person methods

--- apps/handle_data.rb
+++ apps/handle_data.rb
@@ -1,7 +1,13 @@
 require 'json'
+require_relative '../classes/book'
+require_relative '../classes/person'
 
 def initialize_files
   File.write('./data/books.json', '[]') unless File.exist?('./data/books.json')
+  File.zero?('./data/books.json') && File.write('./data/books.json', '[]')
+
+  File.write('./data/persons.json', '[]') unless File.exist?('./data/persons.json')
+  File.zero?('./data/persons.json') && File.write('./data/persons.json', '[]')
 end
 
 def save_books(books)
@@ -21,4 +27,38 @@ def read_books
     books.push(Book.new(book['title'], book['author']))
   end
   books
+end
+
+def save_people(persons)
+  ppl = []
+  persons.each do |person|
+    ppl << if person.instance_of? Student
+             {
+               role: person.class.name,
+               name: person.name,
+               age: person.age
+             }
+           elsif person.instance_of? Teacher
+             {
+               role: person.class.name,
+               name: person.name,
+               age: person.age,
+               specialization: person.specialization
+             }
+           end
+  end
+  File.write('./data/persons.json', JSON.generate(ppl).to_s)
+end
+
+def read_person
+  persons = []
+  JSON.parse(File.read('./data/persons.json')).each do |person|
+    persons << case person['role']
+               when 'Student'
+                 Student.new(person['role'], person['age'], person['name'])
+               when 'Teacher'
+                 Teacher.new(person['age'], person['specialization'], person['name'])
+               end
+  end
+  persons
 end
