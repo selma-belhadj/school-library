2cabb34422b71bd52b936cea90d67e28e34984b8
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-06-22 17:33:50 +0300

    Update checked and fixed all found offences

--- apps/rentals_app.rb
+++ apps/rentals_app.rb
@@ -7,40 +7,32 @@ require_relative './persons_app'
 require 'json'
 
 class RentalsApp
+  attr_accessor :rentals, :books, :persons
 
-    attr_accessor :rentals, :books, :persons
+  def initialize(books, persons)
+    @books = books
+    @persons = persons
+    @rentals = JSON.parse(File.read('./data/rentals.json'))
+  end
 
-    def initialize(books, persons)
-        @books = books
-        @persons = persons
-        @rentals = JSON.parse(File.read('./data/rentals.json'))
-    end
-
-    def add_rental
-        puts '********** Create a new rental **********'
-        puts 'Select which book you want to rent by entering its index: '
-        @books.each_with_index do |book, index|
-            puts "#{index}) Book Title: \"#{book.title}\" | Author: #{book.author}"
-        end
-        book_index = gets.chomp.to_i
-        puts 'Select a person from the list by its index: '
-        @persons.each_with_index do |person, index| 
-            puts " #{index}) [#{person.class.name}] Name: #{person.name} | Age: #{person.age} | ID: #{person.id}" 
-        end
-        person_index = gets.chomp.to_i
-        puts 'Enter date of rentals: [format: yyyy-mm-dd]: '
-        date = gets.chomp.to_s
-        rental = Rental.new(date, @books[book_index], @persons[person_index])
-        rtl = {
-            date: rental.date,
-            name: rental.person.name,
-            title: rental.book.title,
-            author: rental.book.author,
-            person_id: rental.person.id
-        }
-        @rentals << rtl
-        File.write('./data/rentals.json', JSON.generate(@rentals))
-        puts ''
-        puts '********** Rental created successfully **********'
-    end
+  def add_rental
+    puts '********** Create a new rental **********'
+    puts 'Select which book you want to rent by entering its index: '
+    book_index = gets.chomp.to_i
+    puts 'Select a person from the list by its index: '
+    person_index = gets.chomp.to_i
+    puts 'Enter date of rentals: [format: yyyy-mm-dd]: '
+    date = gets.chomp.to_s
+    rental = Rental.new(date, @books[book_index], @persons[person_index])
+    rtl = {
+      date: rental.date,
+      name: rental.person.name,
+      title: rental.book.title,
+      author: rental.book.author,
+      person_id: rental.person.id
+    }
+    @rentals << rtl
+    File.write('./data/rentals.json', JSON.generate(@rentals))
+    puts '********** Rental created successfully **********'
+  end
 end
